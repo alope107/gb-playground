b9a97c157da6ce8038b9a98016e3703790a50256
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-12-20 17:26:00 -0800

    Multiple dots - manual

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int main() {
     bn::core::init();
 
     bn::sprite_ptr dot = bn::sprite_items::dot.create_sprite(0, 0);
+    bn::sprite_ptr dot2 = bn::sprite_items::dot.create_sprite(20, 0);
+    bn::sprite_ptr dot3 = bn::sprite_items::dot.create_sprite(40, 0);
+    bn::sprite_ptr dot4 = bn::sprite_items::dot.create_sprite(60, 0);
 
     while(1) {
         bn::core::update();

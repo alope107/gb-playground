cd1018f9affc701d21dccb4a2f66ad6732e9e57b
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-12-20 17:44:09 -0800

    loggin

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <bn_core.h>
 #include <bn_sprite_ptr.h>
+#include <bn_log.h>
 
 #include <bn_vector.h>
 
@@ -11,6 +12,7 @@ int main() {
     bn::vector<bn::sprite_ptr, 10> dots = {};
 
     for(int x = 0; x <= 100; x+=20) {
+        BN_LOG("x location: ", x);
         bn::sprite_ptr dot = bn::sprite_items::dot.create_sprite(x, 0);
         dots.push_back(dot);
     }

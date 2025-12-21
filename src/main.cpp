56f2772a6e2739831703463040acd63a9c81018b
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-12-20 17:55:39 -0800

    Double loop

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,18 @@
 int main() {
     bn::core::init();
 
-    bn::vector<bn::sprite_ptr, 10> dots = {};
+    bn::vector<bn::sprite_ptr, 36> dots = {};
 
     for(int x = 0; x <= 100; x+=20) {
-        BN_LOG("x location: ", x);
-        bn::sprite_ptr dot = bn::sprite_items::dot.create_sprite(x, 0);
-        bn::fixed scale = 1 + (x/100.0);
-        BN_LOG("scale: ", scale);
-        dot.set_scale(scale);
-        dots.push_back(dot);
+        for(int y = -40; y <= 60; y+= 20) {
+            BN_LOG("x", x, "y", y);
+            bn::sprite_ptr dot = bn::sprite_items::dot.create_sprite(x, y);
+            // bn::fixed scale = 1 + (x/100.0);
+            // BN_LOG("scale: ", scale);
+            // dot.set_scale(scale);
+            dots.push_back(dot);
+        }
+        
     }
 
     while(1) {

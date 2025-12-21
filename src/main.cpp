1e888da627eae9f63483a8bcc8db6f1de90ca0db
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-12-20 19:21:59 -0800

    wrap tribool refactor

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,23 @@
 
 #include "bn_sprite_items_dot.h"
 
+int tribool(bool minus, bool plus) {
+    int val = 0;
+    if(minus) val--;
+    if(plus) val++;
+    return val;
+}
+
+int wrap(int value, int max) {
+    if(value < 0) {
+        value = max;
+    }
+    if (value > max) {
+        value = 0;
+    }
+    return value;
+}
+
 int main() {
     bn::core::init();
 
@@ -18,15 +35,8 @@ int main() {
     for(int y = -80; y <= 80; y+=20) {
         bn::vector<bn::sprite_ptr, 13> new_line = {};
         for(int x = -120; x <= 120; x+=20) {
-            BN_LOG("x", x, "y", y);
             count++;
-            BN_LOG("count ", count);
             bn::sprite_ptr dot = bn::sprite_items::dot.create_sprite(x, y);
-            // if(x > 0) {
-            //     bn::fixed scale = 1 + (x/100.0) + (y/100.0);
-            //     BN_LOG("scale: ", scale);
-            //     dot.set_scale(scale);
-            // }
             new_line.push_back(dot);
         }
         dots.push_back(new_line);
@@ -39,38 +49,10 @@ int main() {
     dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
 
     while(1) {
-        if (bn::keypad::left_pressed()) {
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 0);
-            sel_col--; 
-            if(sel_col < 0) {
-                sel_col = dots[0].size() - 1;
-            }
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
-        }
-        if (bn::keypad::right_pressed()) {
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 0);
-            sel_col++; 
-            if(sel_col >= dots[0].size()) {
-                sel_col = 0;
-            }
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
-        }
-        if (bn::keypad::up_pressed()) {
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 0);
-            sel_row--; 
-            if(sel_row < 0) {
-                sel_row = dots.size() -1;
-            }
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
-        }
-        if (bn::keypad::down_pressed()) {
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 0);
-            sel_row++; 
-            if(sel_row >= dots.size()) {
-                sel_row = 0;
-            }
-            dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
-        }
+        dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 0);
+        sel_col = wrap(sel_col+tribool(bn::keypad::left_pressed(), bn::keypad::right_pressed()), dots[0].size());
+        sel_row = wrap(sel_row+tribool(bn::keypad::up_pressed(), bn::keypad::down_pressed()), dots.size());
+        dots[sel_row][sel_col].set_item(bn::sprite_items::dot, 1);
         bn::core::update();
     }
 

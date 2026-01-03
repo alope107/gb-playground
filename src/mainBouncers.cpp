e6c01ff5dac2a423b4263ae513f7b79008fb3110
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-01-03 11:47:31 -0800

    array version

--- src/mainBouncers.cpp
+++ src/mainBouncers.cpp
@@ -1,51 +1,70 @@
 #include <bn_core.h>
 #include <bn_sprite_ptr.h>
-// #include <bn_vector.h>
+#include <bn_vector.h>
 
 #include <bn_sprite_items_dot.h>
 
 #define HALF_SCREEN_WIDTH 120
 #define HALF_SCREEN_HEIGHT 80 
 
-#define BOUNCER_COUNT 5
+#define BOUNCER_COUNT 3
 
 int main() {
     bn::core::init();
 
-    // bn::vector<bn::sprite_ptr> bouncers
+    bn::array<bn::sprite_ptr, BOUNCER_COUNT> bouncers = {
+        bn::sprite_items::dot.create_sprite(0, 0, 1),
+        bn::sprite_items::dot.create_sprite(0, 0, 1),
+        bn::sprite_items::dot.create_sprite(0, 0, 1)
+    };
 
-    bn::sprite_ptr bouncer = bn::sprite_items::dot.create_sprite(0, 0, 1);
-    bn::sprite_ptr bouncer2 = bn::sprite_items::dot.create_sprite(40, 50, 1);
+    bn::array<bn::fixed, BOUNCER_COUNT> dxs = {
+       1,
+       1.5,
+       -1
+    };
 
-    bn::fixed dx = 1;
-    bn::fixed dy = 1;
-
-    bn::fixed dx2 = -1;
-    bn::fixed dy2 = 1;
+    bn::array<bn::fixed, BOUNCER_COUNT> dys = {
+        2,
+        -1.1,
+        1
+    };
 
     while(true) {
-        bouncer.set_x(bouncer.x() + dx);
-        bouncer.set_y(bouncer.y() + dy);
+        for(int i = 0; i < BOUNCER_COUNT; i++){
+            bouncers[i].set_x(bouncers[i].x() + dxs[i]);
+            bouncers[i].set_y(bouncers[i].y() + dys[i]);
 
-        bouncer2.set_x(bouncer2.x() + dx2);
-        bouncer2.set_y(bouncer2.y() + dy2);
+            if(bouncers[i].x() >= HALF_SCREEN_WIDTH || bouncers[i].x() <= -HALF_SCREEN_WIDTH) {
+                dxs[i] *= -1;
+            }
 
-        if (bouncer.x() >= HALF_SCREEN_WIDTH || bouncer.x() <= -HALF_SCREEN_WIDTH) {
-            dx *= -1;
+            if(bouncers[i].y() >= HALF_SCREEN_HEIGHT || bouncers[i].y() <= -HALF_SCREEN_HEIGHT) {
+                dys[i] *= -1;
+            }
         }
+        // bouncer.set_x(bouncer.x() + dx);
+        // bouncer.set_y(bouncer.y() + dy);
 
-        if (bouncer.y() >= HALF_SCREEN_HEIGHT || bouncer.y() <= -HALF_SCREEN_HEIGHT) {
-            dy *= -1;
-        }
+        // bouncer2.set_x(bouncer2.x() + dx2);
+        // bouncer2.set_y(bouncer2.y() + dy2);
+
+        // if (bouncer.x() >= HALF_SCREEN_WIDTH || bouncer.x() <= -HALF_SCREEN_WIDTH) {
+        //     dx *= -1;
+        // }
+
+        // if (bouncer.y() >= HALF_SCREEN_HEIGHT || bouncer.y() <= -HALF_SCREEN_HEIGHT) {
+        //     dy *= -1;
+        // }
 
 
-        if (bouncer2.x() >= HALF_SCREEN_WIDTH || bouncer2.x() <= -HALF_SCREEN_WIDTH) {
-            dx2 *= -1;
-        }
+        // if (bouncer2.x() >= HALF_SCREEN_WIDTH || bouncer2.x() <= -HALF_SCREEN_WIDTH) {
+        //     dx2 *= -1;
+        // }
 
-        if (bouncer2.y() >= HALF_SCREEN_HEIGHT || bouncer2.y() <= -HALF_SCREEN_HEIGHT) {
-            dy2 *= -1;
-        }
+        // if (bouncer2.y() >= HALF_SCREEN_HEIGHT || bouncer2.y() <= -HALF_SCREEN_HEIGHT) {
+        //     dy2 *= -1;
+        // }
 
         bn::core::update();
     }
